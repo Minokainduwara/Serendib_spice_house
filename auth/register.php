86d4276d26e9d18c846ba55c467803230920a05c
Minoka Wickramasinghe <76654300+Minokainduwara@users.noreply.github.com>
2025-05-10 02:41:20 +0530

    Database Created

--- auth/register.php
+++ auth/register.php
@@ -1,4 +1,6 @@
 <?php require "../includes/header.php" ?>
+<?php require "../config/config.php" ?>
+
     <div class="container">
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
