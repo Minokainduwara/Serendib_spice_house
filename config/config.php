1ce4159fc1d4ef5934f4d60da8796f756997978f
Minoka Wickramasinghe <76654300+Minokainduwara@users.noreply.github.com>
2025-05-10 01:53:11 +0530

    database connected

---
+++ config/config.php
@@ -0,0 +1,18 @@
+<?php
+
+
+//host
+$host = 'localhost';
+
+//dbname
+$dbname = "Spice";
+
+//username
+$user = "root";
+$pass = "";
+
+$conn = new PDO("mysql:host=$host;dbname=$dbname",$user,$pass);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+?>
