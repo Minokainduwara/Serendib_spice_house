c6263a79ccc20ad6fba707a0fa8c48fb2e4f6cbf
Minoka Wickramasinghe <76654300+Minokainduwara@users.noreply.github.com>
2025-05-10 01:55:02 +0530

    connection validation

--- config/config.php
+++ config/config.php
@@ -14,5 +14,11 @@ $pass = "";
 $conn = new PDO("mysql:host=$host;dbname=$dbname",$user,$pass);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+if($conn){
+    echo "Worked Sucessfully";
+}else{
+    echo "database error";
+}
+
 
 ?>
