950ac116cec1fde2cc0706090452d7545e7414d0
Minoka Wickramasinghe <76654300+Minokainduwara@users.noreply.github.com>
2025-05-10 15:31:57 +0530

    error fixed

--- auth/register.php
+++ auth/register.php
@@ -22,7 +22,7 @@
                 ':mypassword' => password_hash($password,PASSWORD_DEFAULT),
             ]);
 
-            header("location: ;login.php");
+            header("location: login.php");
         }
     }
 
