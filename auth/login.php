554447215711feed34c983fa18dcfa24b6d9ef8b
Minoka Wickramasinghe <76654300+Minokainduwara@users.noreply.github.com>
2025-05-10 16:32:29 +0530

    login implemented

--- auth/login.php
+++ auth/login.php
@@ -1,4 +1,23 @@
 <?php require "../includes/header.php" ?>
+
+<?php require "../config/config.php" ?>
+
+
+<?php
+    if(isset($_POST['submit'])){
+        if(empty($_POST['email']) OR empty($_POST['password'])){
+            echo "<script> alert('one or more inputs are empty'); </script>";
+        }else{
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+        }
+
+        $login = $conn->query("SELECT * FROM users WHERE email='$email'");
+        $login->execute();
+    }
+
+
+?>
     <div class="container">
 
 
