333493376545868548e52408fedd9c84a10619c6
Minoka Wickramasinghe <76654300+Minokainduwara@users.noreply.github.com>
2025-05-10 14:40:35 +0530

    register backend

--- auth/register.php
+++ auth/register.php
@@ -1,6 +1,34 @@
 <?php require "../includes/header.php" ?>
 <?php require "../config/config.php" ?>
 
+
+<?php
+
+    //Check form submission
+    if(isset($_POST['submit'])){
+        if(empty($_POST['username']) OR empty($_POST['email']) OR empty($_POST['password'])){
+            echo "<script> alert('one or more inputs are empty'); </script>";
+        }else{
+            $username = $_POST['username'];
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+
+            $insert = $conn-prepare("INSERT INTO users (username, email, mypassword) 
+              VALUES (:username, :email, :mypassword)");
+
+            $insert->execute([
+                ':username' => $username,
+                ':email' => $email,
+                ':mypassword' => $password,
+            ]);
+
+            header("location: ;login.php");
+        }
+    }
+
+
+?>
+
     <div class="container">
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -44,27 +72,27 @@
 
         <div class="row justify-content-center">
             <div class="col-md-6">
-                <form class="form-control mt-5">
+                <form class="form-control mt-5" method ="POST" action = 'register.php'>
                     <h4 class="text-center mt-3"> Register </h4>
                     <div class="">
                         <label for="" class="col-sm-2 col-form-label">Username</label>
                         <div class="">
-                            <input type="text"  class="form-control" id="" value="">
+                            <input type="text" name="username" class="form-control" >
                         </div>
                     </div>
                     <div class="">
                         <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
                         <div class="">
-                            <input type="email"  class="form-control" id="" value="">
+                            <input type="email" name="email" class="form-control" >
                         </div>
                     </div>
                     <div class="">
                         <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
                         <div class="">
-                            <input type="password" class="form-control" id="inputPassword">
+                            <input type="password" class="form-control" name="password" id="inputPassword">
                         </div>
                     </div>
-                    <button class="w-100 btn btn-lg btn-primary mt-4" type="submit">register</button>
+                    <button class="w-100 btn btn-lg btn-primary mt-4" type="submit" name="submit">register</button>
 
                 </form>
             </div>
