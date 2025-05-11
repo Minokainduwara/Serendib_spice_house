cca5321a0318322d5c7b5b831fc53d8f77407abd
Minoka Wickramasinghe <76654300+Minokainduwara@users.noreply.github.com>
2025-05-11 21:52:03 +0530

    2 products added for testing

--- index.php
+++ index.php
@@ -8,24 +8,26 @@
 ?>
 <div class="container">    
         <div class="row mt-5">
-            <div class="col-lg-4 col-md-6 col-sm-10 offset-md-0 offset-sm-1">
-                <div class="card" >
-                    <img height="213px" class="card-img-top" src="images/node.png">
-                    <div class="card-body" >
-                        <h5 class="d-inline"><b>Node Basics</b> </h5>
-                        <h5 class="d-inline"><div class="text-muted d-inline">($10/item)</div></h5>
-                        <p>Monotonectally enable customized 
-                            growth strategies and 24/7 portals.  functional opportunities. </p>
-                         <a href="#"  class="btn btn-primary w-100 rounded my-2"> More<i class="fas fa-arrow-right"></i> </a>      
-     
+                <?php foreach($allRows as $product) : ?>
+                <div class="col-lg-4 col-md-6 col-sm-10 offset-md-0 offset-sm-1">
+                    <div class="card" >
+                        <img height="213px" class="card-img-top" src="images/<?php echo $product->image; ?>">
+                        <div class="card-body" >
+                            <h5 class="d-inline"><b><?php echo $product->name; ?></b> </h5>
+                            <h5 class="d-inline"><div class="text-muted d-inline">($<?php echo $product->price ?>)</div></h5>
+                            <p><?php echo $product->description; ?></p>
+                            <a href="<?php echo APPURL; ?>/shopping/single.php?=id<?php echo $product->id; ?>"  class="btn btn-primary w-100 rounded my-2"> More<i class="fas fa-arrow-right"></i> </a>      
+        
+                        </div>
                     </div>
                 </div>
-            </div>
             <!-- 
             for testing
             <?php echo $_SESSION['username']; ?>
-                
+
             -->
             <br>
+
+            <?php endforeach; ?>
          </div>
 <?php require "includes/footer.php"  ?>
