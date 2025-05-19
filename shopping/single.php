0858e45a9c161fab4a0c77b5c7663ae5545c5a4f
Minokainduwara <minokainduwara@gmail.com>
2025-05-19 23:54:07 +0530

    cart items

--- shopping/single.php
+++ shopping/single.php
@@ -1,5 +1,19 @@
 <?php require "../includes/header.php" ?>
 <?php require "../config/config.php" ?>
+
+<?php 
+    if(isset($_GET['id'])){
+        $id = $_GET['id'];
+        $row = $conn->query("SELECT * FROM products WHERE status = 1 AND id = '$id'");
+        $row->execute();
+
+        $product = $row->fetch(PDO::FETCH_OBJ);
+    } else {
+        header("Location: index.php");
+        exit();
+    }
+
+?>
   <div class="container mt-5 mb-5">
         <div class="row d-flex justify-content-center">
             <div class="col-md-10">
@@ -7,20 +21,20 @@
                     <div class="row">
                         <div class="col-md-6">
                             <div class="images p-3">
-                                <div class="text-center p-4"> <img id="main-image" src="../images/django.png" width="250" /> </div>
+                                <div class="text-center p-4"> <img id="main-image" src="../images/<?php echo $product->image; ?>" width="250" /> </div>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="product p-4">
                                 <div class="d-flex justify-content-between align-items-center">
-                                    <div class="d-flex align-items-center"> <a href="#" class="ml-1 btn btn-primary"><i class="fa fa-long-arrow-left"></i> Back</a> </div> <i class="fa fa-shopping-cart text-muted"></i>
+                                    <div class="d-flex align-items-center"> <a href="<?php echo APPURL; ?>" class="ml-1 btn btn-primary"><i class="fa fa-long-arrow-left"></i> Back</a> </div> <i class="fa fa-shopping-cart text-muted"></i>
                                 </div>
                                 <div class="mt-4 mb-3"> 
-                                    <h5 class="text-uppercase">Django Book</h5>
-                                    <div class="price d-flex flex-row align-items-center"> <span class="act-price">$20</span>
+                                    <h5 class="text-uppercase"><?php echo $product->name; ?></h5>
+                                    <div class="price d-flex flex-row align-items-center"> <span class="act-price"><?php echo $product->price; ?></span>
                                     </div>
                                 </div>
-                                <p class="about">Shop from a wide range of t-shirt from orianz. Pefect for your everyday use, you could pair it with a stylish pair of jeans or trousers complete the look.</p>
+                                <p class="about"><?php echo $product->description; ?></p>
                               
                                 <div class="cart mt-4 align-items-center"> <button class="btn btn-primary text-uppercase mr-2 px-4"><i class="fas fa-shopping-cart"></i> Add to cart</button> </div>
                             </div>
