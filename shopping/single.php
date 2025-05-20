1cc213268acd6e540dbac6acda81445667207769
Minokainduwara <minokainduwara@gmail.com>
2025-05-21 03:50:24 +0530

    user not logged in backend

--- shopping/single.php
+++ shopping/single.php
@@ -70,41 +70,46 @@
 
                                 <form method="post" id="form-data">
                                     <div class="">
-                                        <input type="text" name="pro_id" value="<?php echo $product->id; ?>" class="form-control">
+                                        <input type="hidden" name="pro_id" value="<?php echo $product->id; ?>" class="form-control">
                                     </div>
 
                                     <div class="">
-                                        <input type="text" name="pro_name" value="<?php echo $product->name; ?>" class="form-control">
+                                        <input type="hidden" name="pro_name" value="<?php echo $product->name; ?>" class="form-control">
                                     </div>
 
                                     <div class="">
-                                        <input type="text" name="pro_image" value="<?php echo $product->image; ?>" class="form-control">
+                                        <input type="hidden" name="pro_image" value="<?php echo $product->image; ?>" class="form-control">
                                     </div>
 
                                     <div class="">
-                                        <input type="text" name="pro_price" value="<?php echo $product->price; ?>"  class="form-control">
+                                        <input type="hidden" name="pro_price" value="<?php echo $product->price; ?>"  class="form-control">
                                     </div>
 
                                     <div class="">
-                                        <input type="text" name="pro_amount" value="1"  class="form-control">
+                                        <input type="hidden" name="pro_amount" value="1"  class="form-control">
                                     </div>
 
                                     <div class="">
-                                        <input type="text" name="user_id" value="<?php echo $_SESSION['user_id']; ?>"  class="form-control">
+                                        <input type="hidden" name="pro_file" value="<?php echo $product->file; ?>"  class="form-control">
                                     </div>
-
-                                    <div class="">
-                                        <input type="text" name="pro_file" value="<?php echo $product->file; ?>"  class="form-control">
-                                    </div>
+                                    
+                                    <?php if(isset($_SESSION['user_id'])) : ?>
+                                        <div class="">
+                                            <input type="text" name="user_id" value="<?php echo $_SESSION['user_id']; ?>"  class="form-control">
+                                        </div>
+                                    <?php endif; ?>
+                                    
                                 
                               
                                 <div class="cart mt-4 align-items-center"> 
-                                    <?php if($select->rowcount() > 0) :  ?>
-                                        <button id="submit" name="submit" type="submit" disabled class="btn btn-primary text-uppercase mr-2 px-4"><i class="fas fa-shopping-cart"></i> Add to cart</button> 
-                                    <?php else : ?>
-                                        <button id="submit" name="submit" type="submit" class="btn btn-primary text-uppercase mr-2 px-4"><i class="fas fa-shopping-cart"></i> Add to cart</button> 
-                                    <?php endif; ?>
 
+                                    <?php if(isset($_SESSION['user_id'])) : ?>
+                                        <?php if($select->rowcount() > 0) :  ?>
+                                            <button id="submit" name="submit" type="submit" disabled class="btn btn-primary text-uppercase mr-2 px-4"><i class="fas fa-shopping-cart"></i> Add to cart</button> 
+                                        <?php else : ?>
+                                            <button id="submit" name="submit" type="submit" class="btn btn-primary text-uppercase mr-2 px-4"><i class="fas fa-shopping-cart"></i> Add to cart</button> 
+                                        <?php endif; ?>
+                                    <?php endif; ?>
                                 </div>
                                 </form>
                             </div>
